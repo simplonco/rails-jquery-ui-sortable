5bd9ee163e5efa8b209b32a5673dcb743174d349
remy <remy@Ruff.(none)>
2014-02-17 12:47:03 +0100

    first real commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SortableList::Application.config.secret_key_base = '3e6eab2c73bbd6e20898ed2d0742f873d5222b0e66f412e62cd7c16a3aa05a33e4e43bdabffa103e31d8a478ad5291cc8720f5bf74b9e296e24a242061f2e8eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SortableList::Application.config.session_store :cookie_store, key: '_sortable_list_session'
